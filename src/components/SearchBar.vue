3855c6b1c7201f5200fc9b87d377de3811d10ba8
isaacwrk <isaacsouzawrk@gmail.com>
2022-01-20 16:19:08 -0300

    fixing search bar padding

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex flex-1 mt-7 justify-items-start ml-5">
         <div class="flex border-2 rounded-xl w-81 h-10">
-            <button class="flex items-center justify-center px-4">
+            <button class="flex items-center justify-center px-4 pl-3">
                 <i :class="icon" class="text-gray-400"></i>
             </button>
-            <input type="text" class="px-4 py-2 w-80" :placeholder="searchText">
+            <input type="text" class=" py-2 pl-2 w-80" :placeholder="searchText">
             <i class="fa fa-sliders-h text-gray-400 hover:text-green-500 hover:cursor-pointer mt-3 mr-4"></i>
         </div>
     </div>
