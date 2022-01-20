aae0a659acc7fb0532ae428560ad616236bd9265
isaacwrk <isaacsouzawrk@gmail.com>
2022-01-20 16:00:33 -0300

    Creating search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="flex flex-1 mt-10 justify-center">
+        <div class="flex border-2 rounded w-72 h-10">
+            <button class="flex items-center justify-center px-4">
+                <i :class="icon" class="text-gray-400"></i>
+            </button>
+            <input type="text" class="px-4 py-2 w-80" :placeholder="searchText">
+            <i class="fa fa-sliders-h text-gray-400 hover:text-green-500 hover:cursor-pointer mt-3 mr-4"></i>
+        </div>
+    </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const SearchBar = defineComponent({
+    props:{
+        searchText:{
+            type:String,
+            default:'Search...'
+        },
+        icon:{
+            type:String,
+            default:'fa fa-search'
+        }
+    }
+});
+
+export default SearchBar;
+</script>
+
+<style>
+
+</style>
