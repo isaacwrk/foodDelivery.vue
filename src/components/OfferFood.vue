5386c00c8031a0683f07b7b1fe6dfbec4c2a9bed
isaacwrk <isaacsouzawrk@gmail.com>
2022-01-23 11:46:13 -0300

    start to creating OfferFood component

---
+++ src/components/OfferFood.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 class="ml-5">{{title}}</h1>
+        <div class="w-36 h-53 bg-background-gray">
+
+        </div>
+    </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const OfferFood = defineComponent({
+    props:{
+        title:{
+            type:String,
+            required:true
+        }
+    }
+});
+
+export default OfferFood;
+</script>
+
+<style>
+
+</style>
