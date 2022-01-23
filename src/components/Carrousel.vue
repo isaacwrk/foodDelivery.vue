b3851ca2402d4b13edf265e8a7494181884d155a
isaacwrk <isaacsouzawrk@gmail.com>
2022-01-23 10:17:59 -0300

    creating carrousel component

---
+++ src/components/Carrousel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="whitespace-nowrap overflow-x-auto">
+        <slot/>
+    </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+
+const Carrousel = defineComponent({
+});
+
+export default Carrousel;
+</script>
+
+<style>
+
+</style>
