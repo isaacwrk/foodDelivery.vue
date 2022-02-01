3d4c5b474f140d37b6c1596863bcb9908ca9d783
isaacwrk <isaacsouzawrk@gmail.com>
2022-02-01 20:10:42 -0300

    Adding color text scheme to top of weekc card

--- src/components/TopOfWeek.vue
+++ src/components/TopOfWeek.vue
@@ -2,6 +2,11 @@
     <div class="h-25 ml-5 mt-3 bg-background-gray text-center rounded-lg mx-3 relative">
         <img src="~@/assets/img/topofweek1.png" class="w-24 h-24 ml-3 object-contain" />
         <div class="absolute right-4 top-3"><i class="fas fa-heart text-secondary-black"></i></div>
+        <div class="absolute left-32 top-3 text-left">
+            <p class="text-primary-black text-xl">oioioi</p>
+            <p class="text-secondary-black">oioioiio</p>
+            <p class="text-secondary-black text-sm">opio</p>
+        </div>
     </div>
 </template>
 
